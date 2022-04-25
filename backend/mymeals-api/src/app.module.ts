ba2855a2090cc178e4279518ae08801243e14276
Lucas Weslen Lopes de Matos <matoslopes23@gmail.com>
2022-04-24 20:09:00 -0400

    feat: create tables of system

--- backend/mymeals-api/src/app.module.ts
+++ backend/mymeals-api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { CategoryModule } from './modules/category/category.module';
+import { FoodModule } from './modules/food/food.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, CategoryModule, FoodModule],
   controllers: [AppController],
   providers: [AppService],
 })
