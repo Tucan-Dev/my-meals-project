6605d2ded9d86dc05bb1ff5f9875cdf360fb080b
Lucas Weslen Lopes de Matos <matoslopes23@gmail.com>
2022-04-24 20:43:54 -0400

    refactor: update readme

--- backend/mymeals-api/src/app.module.ts
+++ backend/mymeals-api/src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './modules/auth/auth.module';
-import { UserModule } from './modules/user/user.module';
-import { CategoryModule } from './modules/category/category.module';
-import { FoodModule } from './modules/food/food.module';
+
 
 @Module({
-  imports: [AuthModule, UserModule, CategoryModule, FoodModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
